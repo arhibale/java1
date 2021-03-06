a52520883944385a1f0f01cfc18dd8660aae8065
ArhiBale <arhibale@yandex.ru>
2021-03-07 02:02:30 +0400

    added win verification

--- src/ru/geekbrains/homework4/Main.java
+++ src/ru/geekbrains/homework4/Main.java
@@ -19,32 +19,108 @@ public class Main {
     public static void main(String[] args) {
         initMap();
         printMap();
-        theMainGameCycle();
-    }
-
-    public static void initMap() {
-        map = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                map[i][j] = DOT_EMPTY;
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
             }
         }
+        System.out.println("Игра закончена");
     }
 
-    public static void printMap() {
-        for (int i = 0; i <= SIZE; i++) {
-            System.out.print(i + " ");
+    public static boolean checkWin(char symbol) {
+        if (verticals(symbol)) {
+            return true;
+        } else {
+            return diagonals(symbol);
         }
-        System.out.println();
-        for (int i = 0; i < SIZE; i++) {
-            System.out.print((i + 1) + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
+
+    public static boolean verticals(char symbol) {
+        int mapLength = map.length - 1;
+        int dotsToWin = 0;
+        for (int a = 0; a <= mapLength; a++) {
+            for (int b = 0; b <= mapLength; b++) {
+                if (map[a][b] == symbol) {
+                    dotsToWin++;
+                    if (dotsToWin == DOTS_TO_WIN) {
+                        return true;
+                    }
+                } else {
+                    dotsToWin = 0;
+                }
+            }
+            dotsToWin = 0;
+            for (int b = 0; b <= mapLength; b++) {
+                if (map[b][a] == symbol) {
+                    dotsToWin++;
+                    if (dotsToWin == DOTS_TO_WIN) {
+                        return true;
+                    }
+                } else {
+                    dotsToWin = 0;
+                }
+            }
+            dotsToWin = 0;
+        }
+        return false;
+    }
+
+    public static boolean diagonals(char symbol) {
+        int mapLength = map.length - 1;
+        int dotsToWin = 0;
+        for (int a = 0; a <= mapLength; a++) {
+            for (int b = 0; b <= mapLength; b++) {
+                if (map[b][b] == symbol) {
+                    dotsToWin++;
+                    if (dotsToWin == DOTS_TO_WIN) {
+                        return true;
+                    }
+                } else {
+                    dotsToWin = 0;
+                }
+            }
+            dotsToWin = 0;
+            for (int b = 0; b <= mapLength; b++) {
+                if (map[b][mapLength - b] == symbol) {
+                    dotsToWin++;
+                    if (dotsToWin == DOTS_TO_WIN) {
+                        return true;
+                    }
+                } else {
+                    dotsToWin = 0;
+                }
+            }
+            dotsToWin = 0;
+        }
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
     public static void aiTurn() {
         int x, y;
         do {
@@ -70,50 +146,27 @@ public class Main {
         return map[y][x] != DOT_EMPTY;
     }
 
-    public static boolean checkWin(char symb) {
-        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
-        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
-        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
-        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
-        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
-        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
-        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
-        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
-        return false;
-    }
-
-    public static boolean isMapFull() {
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == DOT_EMPTY) return false;
+                map[i][j] = DOT_EMPTY;
             }
         }
-        return true;
     }
 
-    public static void theMainGameCycle() {
-        while (true) {
-            humanTurn();
-            printMap();
-            if (checkWin(DOT_X)) {
-                System.out.println("Победил человек");
-                break;
-            }
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
-            }
-            aiTurn();
-            printMap();
-            if (checkWin(DOT_O)) {
-                System.out.println("Победил Искуственный Интеллект");
-                break;
-            }
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
-            }
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
         }
-        System.out.println("Игра закончена");
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
